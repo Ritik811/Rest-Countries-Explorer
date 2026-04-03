4a26ea6c01a4b5826d7ec2356a0980d7c7d07538
Ritik811 <ritikkumar32855@gmail.com>
2026-04-03 17:26:25 +0530

    Country Page is Done

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,3 +1,30 @@
+import { startTransition, useEffect, useState, useTransition } from "react";
+import { getCountryData } from "../api/getApi";
+import { Loader } from "../components/UI/Loader";
+import { CountryCard } from "../components/UI/CountryCard";
+
 export const Country = () => {
-  return <h1>Country Page</h1>;
+  const [isPending, startTransition] = useTransition();
+  const [country, setCountry] = useState([]);
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
+      console.log(res.data);
+      setCountry(res.data);
+    });
+  }, []);
+
+  if (isPending) {
+    return <Loader />;
+  }
+
+  return (
+    <section className="country-section">
+      <ul className="grid grid-four-cols">
+        {country.map((curCountry, index) => {
+          return <CountryCard curCountry={curCountry} key={index} />;
+        })}
+      </ul>
+    </section>
+  );
 };
